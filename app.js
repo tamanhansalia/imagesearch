b083ab644f794b170566c2da8701e9b2f3725087
Taman Hansalia <Taman Hansalia>
2024-04-10 11:32:49 +0530

    searchimage

---
+++ app.js
@@ -0,0 +1,48 @@
+const accesskey = "hsxzmIne4VhBcuhvQPieGv5158k2xMi3gn-YEg-g1k8";
+
+
+const searchform = document.getElementById("search-form");
+const searchbox = document.getElementById("search-box");
+const searchresult = document.getElementById("search-result");
+const showmorebtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchimage() {
+    keyword = searchbox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1){
+        searchresult.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((result)=>
+    {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imagelink = document.createElement("a");
+        imagelink.href = result.links.html;
+        imagelink.target = "_blank";
+        
+
+        // append child property is to use to a tag is inside this img 
+        imagelink.appendChild(image);
+        searchresult.appendChild(imagelink);
+    })
+    showmorebtn.style.display = "block";
+}
+searchform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchimage();
+});
+showmorebtn.addEventListener( "click" , ()=>{
+    page++;
+    searchimage()
+})
